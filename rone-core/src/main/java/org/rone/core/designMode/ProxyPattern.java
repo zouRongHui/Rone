81a218827fdbd6795154890e971e862cba955fea
zouRongHui <zou_rong_hui@foxmail.com>
2022-12-09 17:10:57 +0800

    JDK 动态代理、Cglib 动态代理

--- rone-core/src/main/java/org/rone/core/designMode/ProxyPattern.java
+++ rone-core/src/main/java/org/rone/core/designMode/ProxyPattern.java
@@ -1,6 +1,11 @@
 package org.rone.core.designMode;
 
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
 
@@ -17,6 +22,19 @@ public class ProxyPattern {
         demo = new DemoProxy(demo).getDemo();
         String result = demo.test("rone", 123);
         System.out.println("Result: " + result);
+        System.out.println("###############################################################################");
+
+        // 动态代理
+        Demo demo1 = new DemoImpl();
+        DemoInvocationHandler handler = new DemoInvocationHandler(demo1);
+        Demo proxy = (Demo) handler.getProxy();
+        System.out.println("动态代理执行结果：" + proxy.test("动态代理", 333));
+        System.out.println("###############################################################################");
+
+        CglibProxy cglibProxy = new CglibProxy();
+        //通过生成子类的方式创建代理类
+        DemoImpl proxyImp = (DemoImpl)cglibProxy.getProxy(DemoImpl.class);
+        System.out.println("cglib代理执行结果：" + proxyImp.test("cglib代理", 444));
     }
 
     interface Demo {
@@ -31,7 +49,7 @@ public class ProxyPattern {
         }
     }
 
-    static class DemoProxy {
+    private static class DemoProxy {
 
         private final Demo demo;
 
@@ -41,17 +59,14 @@ public class ProxyPattern {
 
         public Demo getDemo() {
             Demo proxy;
-
             ClassLoader loader = demo.getClass().getClassLoader();
             Class<?>[] interfaces = new Class[] {Demo.class};
             InvocationHandler h = (proxy1, method, args) -> {
                 String methodName = method.getName();
                 //打印日志
                 System.out.println("[before] The method " + methodName + " begins with " + Arrays.asList(args));
-
                 //调用目标方法
                 Object result = null;
-
                 try {
                     //前置通知
                     result = method.invoke(demo, args);
@@ -60,18 +75,68 @@ public class ProxyPattern {
                     e.printStackTrace();
                     //异常通知, 可以访问到方法出现的异常
                 }
-
                 //后置通知. 因为方法可以能会出异常, 所以访问不到方法的返回值
-
                 //打印日志
                 System.out.println("[after] The method ends with " + result);
-
                 return result;
             };
 
             proxy = (Demo) Proxy.newProxyInstance(loader, interfaces, h);
 
             return proxy;
+        }
+    }
+
+    /**
+     * JDK 动态代理：java.lang.reflect 包中的 Proxy 类
+     *      和 InvocationHandler 接口提供了生成动态代理类的能力。
+     * JDK 的动态代理有一个限制，就是使用动态代理的对象必须实现一个或多个接口。
+     */
+    private static class DemoInvocationHandler implements InvocationHandler {
+        private Object target;
+
+        public DemoInvocationHandler(Object target) {
+            super();
+            this.target = target;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            System.out.println("-----------------begin " + method.getName() + "-----------------");
+            Object result = method.invoke(target, args);
+            System.out.println("-----------------end " + method.getName() + "-----------------");
+            return result;
+        }
+
+        public Object getProxy() {
+            return Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), target.getClass().getInterfaces(), this);
+        }
+    }
+
+    /**
+     * Cglib 动态代理：Cglib (Code Generation Library )是一个第三方代码生成类库，
+     *      运行时在内存中动态生成一个子类对象从而实现对目标对象功能的扩展。
+     * 使用 cglib 代理的对象则无需实现接口，达到代理类无侵入。
+     */
+    private static class CglibProxy implements MethodInterceptor {
+        private Enhancer enhancer = new Enhancer();
+
+        public Object getProxy(Class clazz) {
+            //设置需要创建子类的类
+            enhancer.setSuperclass(clazz);
+            enhancer.setCallback(this);
+            //通过字节码技术动态创建子类实例
+            return enhancer.create();
+        }
+
+        //实现 MethodInterceptor 接口方法
+        public Object intercept(Object obj, Method method, Object[] args,
+                                MethodProxy proxy) throws Throwable {
+            System.out.println("前置代理");
+            //通过代理类调用父类中的方法
+            Object result = proxy.invokeSuper(obj, args);
+            System.out.println("后置代理");
+            return result;
         }
     }
 
